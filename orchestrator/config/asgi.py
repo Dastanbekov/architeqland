6461b27b6c44c1290ac98b1f8175f065c95729b8
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2026-07-05 22:50:33 +0500

    fix: add custom JWT auth middleware for websocket to fix auth failure

--- orchestrator/config/asgi.py
+++ orchestrator/config/asgi.py
@@ -9,12 +9,12 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from generation.middleware import JWTAuthMiddlewareStack
 import generation.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddlewareStack(
         URLRouter(
             generation.routing.websocket_urlpatterns
         )
